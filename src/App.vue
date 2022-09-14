3a9f746540fbad6bba3637103399cfc458f0ac85
Carlos Alberto Ribas Junior <carlos.ribas@iesde.com.br>
2022-09-14 18:00:52 -0300

    dev

--- src/App.vue
+++ src/App.vue
@@ -4,33 +4,49 @@
 
 <template>
   <header>
-    <HeaderComponent />
-    <IconComponent :fileName="'nodeIcon.png'" />
-    <IconComponent :fileName="'javascriptIcon.png'" />
-    <IconComponent :fileName="'PythonIcon.png'" />
+    <!-- <HeaderComponent /> -->
+    <!-- <IconComponent :fileName="'nodeIcon.png'" /> -->
+    <!-- <IconComponent :fileName="'javascriptIcon.png'" /> -->
+    <!-- <IconComponent :fileName="'PythonIcon.png'" /> -->
 
-    
+    <button @click="getProjects" >Pedir projetos</button>
   </header>
 
   <main>
-  
+    <div v-if="projects">
+      <ProjectComponent v-for=" project in projects" :key="project._id"/>
+    </div>
+    
   </main>
 </template>
 <script>
   import IconComponent from './components/Icon.vue'
   import HeaderComponent from './components/Header.vue'
+  import ProjectComponent from './components/Project.vue'
   export default {
-    components : {IconComponent, HeaderComponent},
+    components : {IconComponent, HeaderComponent, ProjectComponent},
     data(){
       return {
-
+        projects:null,
       }
     },
     methods : {
-
+      async getProjects(){
+        try {
+          const url = 'http://localhost:9999/list-local';
+          const paramsConfig = {method:'GET', mode:'cors'};
+          const res = await fetch(url, paramsConfig)
+          const {data} = await res.json()
+          return data
+        } catch (error) {
+          console.log(error)
+          return {error, message:"Erro"}
+        }
+      }
     },
-    mounted(){
-        console.log('oi')
+    async mounted(){
+        this.projects = await this.getProjects()
+        console.log(this.projects)
     }
   }
 </script>
---
+++ src/components/Project.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <pre>
+            {{project}}
+        </pre>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    components : {
+
+    },
+    props:{
+        project:{
+            required:false
+        }
+    },
+    data(){
+        return {
+
+        }
+    },
+    mounted(){
+       
+    }
+}
+</script>
+
+<style>
+
+</style>
